3e4768650ec95eb9c671980a7e99e63492c4360a
Bohdan <vitiukbohdan@gmail.com>
2024-02-11 15:34:17 +0200

    second empty function
    Signed-off-by: Bohdan <vitiukbohdan@gmail.com>

--- math_lib/inc/math_lib.h
+++ math_lib/inc/math_lib.h
@@ -1,2 +1,3 @@
 
 void some_function_on_dev_2();
+void second_function_on_dev_2();
--- math_lib/src/math_lib.c
+++ math_lib/src/math_lib.c
@@ -2,3 +2,6 @@ void some_function_on_dev_2()
 {
 	// some empty implementation
 }
+
+void second_function_on_dev_2()
+{}
